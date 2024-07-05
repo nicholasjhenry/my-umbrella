5d1fe749a0712e038d816aca29d0359acc29b176
Nicholas Henry <nicholas.henry@gmail.com>
2024-07-05 14:38:10 -0400

    Stub non-raining case

--- lib/my_umbrella.ex
+++ lib/my_umbrella.ex
@@ -1,2 +1,21 @@
 defmodule MyUmbrella do
+  @moduledoc """
+  A context module.
+  """
+
+  defmodule Announcement do
+    @moduledoc """
+    A type for the announcement in regards to the weather conditions.
+    """
+
+    @type t :: String.t()
+  end
+
+  @type location :: {float(), float()}
+
+  @spec for_today(location, module()) :: {:ok, Announcement.t()} | {:error, term}
+  def for_today(location, weather_api) do
+    {:ok, _weather} = weather_api.get_forecast(location, :today)
+    {:ok, "Nah, all good"}
+  end
 end
--- test/my_umbrella_test.exs
+++ test/my_umbrella_test.exs
@@ -1,3 +1,10 @@
 defmodule MyUmbrellaTest do
   use ExUnit.Case
+
+  describe "determine if an umbrella is needed today" do
+    test "given it is NOT raining before end-of-day; then an umbrella is needed" do
+      orlando = {28.5383, -81.3792}
+      assert {:ok, "Nah, all good"} == MyUmbrella.for_today(orlando, WeatherApi)
+    end
+  end
 end
