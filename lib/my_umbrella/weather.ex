01030e636a2ff1955fd6ad361a71bbd488cca4f5
Nicholas Henry <nicholas.henry@gmail.com>
2024-07-14 10:45:57 -0400

    Determine most intense percipition condition
    Refactor required to add condition to weather type

--- lib/my_umbrella/weather.ex
+++ lib/my_umbrella/weather.ex
@@ -12,10 +12,29 @@ defmodule MyUmbrella.Weather do
           code: integer()
         }
 
+  @precipitation_conditions [
+    snow: 600..622,
+    thunderstorm: 200..232,
+    rain: 500..531,
+    drizzle: 300..321
+  ]
+
   @spec eq?(t(), t()) :: boolean()
   def eq?(lhs, rhs) do
     lhs.coordinates == rhs.coordinates &&
       DateTime.compare(lhs.datetime, rhs.datetime) == :eq &&
       lhs.code == rhs.code
+  end
+
+  def determine_most_intense_precipitation_condition(weather_data) do
+    weather_data
+    |> Enum.filter(&perciptation?/1)
+    |> List.first()
+  end
+
+  defp perciptation?(weather) do
+    @precipitation_conditions
+    |> Keyword.values()
+    |> Enum.any?(&(weather.code in &1))
   end
 end
---
+++ test/my_umbrella/weather_test.exs
@@ -0,0 +1,51 @@
+defmodule MyUmbrella.WeatherApi.WeatherTest do
+  use ExUnit.Case
+
+  alias MyUmbrella.Weather
+  alias MyUmbrella.Coordinates
+
+  describe "determining the most intense precipitation condition" do
+    test "given empty list; then returns nothing" do
+      weather_data = []
+      weather = Weather.determine_most_intense_precipitation_condition(weather_data)
+      refute weather
+    end
+
+    test "given a single weather report with precipitation; then returns that weather report" do
+      london = Coordinates.new(51.5098, -0.118)
+      utc_2130 = ~U[2000-01-01 21:30:00Z]
+      rain = 500
+
+      single_weather = %Weather{
+        coordinates: london,
+        datetime: utc_2130,
+        code: rain
+      }
+
+      weather_data = [single_weather]
+      actual_weather = Weather.determine_most_intense_precipitation_condition(weather_data)
+      assert Weather.eq?(actual_weather, single_weather)
+    end
+
+    test "given a single weather report with no precipitation; then returns nothing" do
+      london = Coordinates.new(51.5098, -0.118)
+      utc_2130 = ~U[2000-01-01 21:30:00Z]
+      clear_sky = 800
+
+      single_weather = %Weather{
+        coordinates: london,
+        datetime: utc_2130,
+        code: clear_sky
+      }
+
+      weather_data = [single_weather]
+      weather = Weather.determine_most_intense_precipitation_condition(weather_data)
+
+      refute weather
+    end
+
+    test "given a multiple weather reports with precipitation; then returns the most intense weather report" do
+      # TODO: pending
+    end
+  end
+end
