6dbb1b490097cd523a098c52fe4be5cc6ed8f217
Nicholas Henry <nicholas.henry@gmail.com>
2024-07-21 13:25:29 -0400

    Format

--- lib/my_umbrella/weather_report.ex
+++ lib/my_umbrella/weather_report.ex
@@ -25,6 +25,20 @@ defmodule MyUmbrella.WeatherReport do
     drizzle: 300..321
   ]
 
+  @spec determine_most_intense_precipitation_condition(list(t())) :: t() | nil
+  def determine_most_intense_precipitation_condition(weather_reports) do
+    weather_reports
+    |> Enum.filter(&perciptation?/1)
+    |> Enum.sort_by(& &1, {:desc, __MODULE__})
+    |> List.first()
+  end
+
+  defp perciptation?(weather) do
+    @precipitation_conditions
+    |> Keyword.values()
+    |> Enum.any?(&(weather.code in &1))
+  end
+
   @spec compare(t, t) :: :eq | :gt | :lt
   def compare(lhs, rhs) do
     weighted_condition =
@@ -48,19 +62,5 @@ defmodule MyUmbrella.WeatherReport do
     lhs.coordinates == rhs.coordinates &&
       DateTime.compare(lhs.datetime, rhs.datetime) == :eq &&
       lhs.code == rhs.code
-  end
-
-  @spec determine_most_intense_precipitation_condition(list(t())) :: t() | nil
-  def determine_most_intense_precipitation_condition(weather_reports) do
-    weather_reports
-    |> Enum.filter(&perciptation?/1)
-    |> Enum.sort_by(& &1, {:desc, __MODULE__})
-    |> List.first()
-  end
-
-  defp perciptation?(weather) do
-    @precipitation_conditions
-    |> Keyword.values()
-    |> Enum.any?(&(weather.code in &1))
   end
 end
