6c308d6e198d628c6ea3d0d5d055837f16ca5c73
Nicholas Henry <nicholas.henry@gmail.com>
2024-07-27 16:29:37 -0400

    Demonstrate async

--- lib/my_umbrella.ex
+++ lib/my_umbrella.ex
@@ -11,7 +11,18 @@ defmodule MyUmbrella do
   @spec for_today(Coordinates.t()) :: {:ok, Precipitation.t()} | {:error, term}
   @spec for_today(Coordinates.t(), DateTime.t()) :: {:ok, Precipitation.t()} | {:error, term}
   def for_today(coordinates, current_date_time_utc \\ DateTime.utc_now()) do
-    with {:ok, response} <- WeatherApi.get_forecast(coordinates, :today),
+    # > Note: if you're running on Elixir 1.8.0 or greater and your concurrency comes from a Task
+    # > then you don't need to add explicit allowances. Instead $callers is used to determine the
+    # > process that actually defined the expectations.
+    # -- https://hexdocs.pm/mox/Mox.html#module-explicit-allowances
+
+    # This is not required, but ensures a solution supports concurrency
+    weather_api_task =
+      Task.async(fn ->
+        WeatherApi.get_forecast(coordinates, :today)
+      end)
+
+    with {:ok, response} <- Task.await(weather_api_task),
          {:ok, weather_report} <- WeatherApi.Response.to_weather_report(response),
          current_date_time_in_time_zone =
            DateTime.shift_zone!(current_date_time_utc, weather_report.time_zone),
