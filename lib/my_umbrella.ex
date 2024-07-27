2e746cf356e451afad1f330a394ac639361ba57a
Nicholas Henry <nicholas.henry@gmail.com>
2024-07-27 10:53:22 -0400

    Integrate filter

--- lib/my_umbrella.ex
+++ lib/my_umbrella.ex
@@ -3,6 +3,7 @@ defmodule MyUmbrella do
   A context module.
   """
 
+  alias MyUmbrella.WeatherReport
   alias MyUmbrella.Coordinates
   alias MyUmbrella.Precipitation
   alias MyUmbrella.Weather
@@ -18,10 +19,14 @@ defmodule MyUmbrella do
 
   @spec for_today(Coordinates.t(), module()) :: {:ok, Announcement.t()} | {:error, term}
   def for_today(coordinates, weather_api) do
+    current_date_time = ~U[2000-01-01 21:30:00Z]
+
     with {:ok, response} <- weather_api.get_forecast(coordinates, :today),
          {:ok, weather_report} <- WeatherApi.Response.to_weather_report(response),
+         weather_report_for_today <-
+           WeatherReport.filter_by_same_day(weather_report, current_date_time),
          %Weather{} = _weather <-
-           Precipitation.determine_most_intense_precipitation_condition(weather_report) do
+           Precipitation.determine_most_intense_precipitation_condition(weather_report_for_today) do
       {:ok, "Nah, all good"}
     end
   end
