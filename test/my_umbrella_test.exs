622c278730b98e1c05d9b10f4332012ccf9c22ec
Nicholas Henry <nicholas.henry@gmail.com>
2024-07-30 16:14:29 -0400

    Use signature shielding

--- test/my_umbrella_test.exs
+++ test/my_umbrella_test.exs
@@ -12,17 +12,10 @@ defmodule MyUmbrellaTest do
   describe "determine if an umbrella is needed today" do
     test "given it IS raining before end-of-day; then an umbrella IS needed" do
       london = coordinates_fixture(:london)
-      time_zone_utc = "Etc/UTC"
-      current_date_time_utc = DateTime.new!(~D[2000-01-01], ~T[21:30:00Z], time_zone_utc)
+      current_date_time = DateTime.new!(~D[2000-01-01], ~T[21:30:00Z], "Etc/UTC")
 
-      expect(MyUmbrella.WeatherApi.Mock, :get_forecast, fn coordinates,
-                                                           :today,
-                                                           _test_server_url ->
-        assert london == coordinates
-        response_fixture(:precipitation, coordinates, time_zone_utc)
-      end)
-
-      weather_result = MyUmbrella.for_today(london, current_date_time_utc)
+      weather_result =
+        determine_if_umbrella_needed_today(:precipitation, london, current_date_time)
 
       expected_weather =
         Weather.new(date_time: ~U[2000-01-01 22:00:00Z], condition: :rain, code: 501)
@@ -33,38 +26,39 @@ defmodule MyUmbrellaTest do
 
     test "given it IS NOT raining before end-of-day; then an umbrella IS NOT needed" do
       orlando = coordinates_fixture(:orlando)
-      time_zone_new_york = "America/New_York"
+      current_date_time = DateTime.new!(~D[2000-01-01], ~T[21:30:00Z], "America/New_York")
 
-      current_date_time_utc =
-        DateTime.new!(~D[2000-01-01], ~T[21:30:00Z], time_zone_new_york)
-        |> DateTime.shift_zone!("Etc/UTC")
-
-      expect(MyUmbrella.WeatherApi.Mock, :get_forecast, fn coordinates,
-                                                           :today,
-                                                           _test_server_url ->
-        assert orlando == coordinates
-        response_fixture(:no_precipitation, coordinates, time_zone_new_york)
-      end)
-
-      weather_result = MyUmbrella.for_today(orlando, current_date_time_utc)
+      weather_result =
+        determine_if_umbrella_needed_today(:no_precipitation, orlando, current_date_time)
 
       assert {:ok, :no_precipitation} == weather_result
     end
 
     test "given an HTTP response with an error; then the error is returned" do
       london = coordinates_fixture(:london)
-      time_zone_utc = "Etc/UTC"
-      current_date_time_utc = DateTime.new!(~D[2000-01-01], ~T[21:30:00Z], time_zone_utc)
+      current_date_time_utc = DateTime.new!(~D[2000-01-01], ~T[21:30:00Z], "Etc/UTC")
 
-      expect(MyUmbrella.WeatherApi.Mock, :get_forecast, fn _coordinates,
-                                                           :today,
-                                                           _test_server_url ->
-        response_fixture(:error, london, time_zone_utc)
-      end)
-
-      weather_result = MyUmbrella.for_today(london, current_date_time_utc)
+      weather_result =
+        determine_if_umbrella_needed_today(:error, london, current_date_time_utc)
 
       assert {:error, {:status, 401}} == weather_result
+    end
+
+    defp determine_if_umbrella_needed_today(
+           maybe_precipitation,
+           coordinates,
+           current_date_time
+         ) do
+      current_date_time_utc = DateTime.shift_zone!(current_date_time, "Etc/UTC")
+
+      expect(MyUmbrella.WeatherApi.Mock, :get_forecast, fn mock_coordinates,
+                                                           :today,
+                                                           _test_server_url ->
+        assert coordinates == mock_coordinates
+        response_fixture(maybe_precipitation, coordinates, current_date_time.time_zone)
+      end)
+
+      MyUmbrella.for_today(coordinates, current_date_time_utc)
     end
   end
 end
