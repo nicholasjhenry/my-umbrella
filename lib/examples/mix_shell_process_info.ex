adf635c68582b2fc43a96169eed6d1aa396654ba
Nicholas Henry <nicholas.henry@gmail.com>
2024-08-16 10:34:36 -0400

    Delint

--- lib/examples/mix_shell_process_info.ex
+++ lib/examples/mix_shell_process_info.ex
@@ -1,10 +1,14 @@
 defmodule Examples.Mix.Shell.Process do
+  @moduledoc """
+  Example code for `Mix.Shell.Process`.
+  """
+
   def info(message) do
     send(message_target(), {:mix_shell, :info, [message]})
     :ok
   end
 
-  defp message_target() do
+  defp message_target do
     case Process.get(:"$callers") do
       [parent | _] -> parent
       _ -> self()
